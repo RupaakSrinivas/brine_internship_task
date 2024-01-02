89dbdf42418278df8bf68522f2a15ad059dca553
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2024-01-02 18:37:05 +0530

    feat: added favourites and toggle cart status on product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
-import { useUser, ProductData } from "../context";
+import { useUser, ProductData, FavoritesData } from "../context";
 
 export default function ProductPage() {
   const { id } = useParams<{ id: string }>();
-  const { user, updateCart } = useUser();
+  const { user, updateCart, updateFavorites } = useUser();
+  const [ isFavorite, setIsFavorite ] = useState(false);
+  const [ cartStatus, setCartStatus ] = useState(false);
 
   const baseUrl = process.env.REACT_APP_API_BASEURL;
   const [product, setProduct] = useState<ProductData>({
@@ -26,10 +28,26 @@ export default function ProductPage() {
           setProduct(response.data);
         }
       });
+      const favorites = user?.favorites.favoriteitems || [];
+      const favitem = favorites.find((item) => item.id === product.id);
+      if (favitem) {
+        setIsFavorite(true);
+      } else {
+        setIsFavorite(false);
+      }
+      const cart = user?.cart || { items: [], price: 0 };
+      const items = cart.items;
+      const item = items.find((item) => item.id === product.id);
+      if (item) {
+        setCartStatus(true);
+      } else {
+        setCartStatus(false);
+      }
     } catch (error) {
       window.alert(error);
     }
-  }, [id]);
+    
+  }, [id, user ]);
 
   const handleAddItemCart = () => {
     const cart = user?.cart || { items: [], price: 0 };
@@ -60,6 +78,31 @@ export default function ProductPage() {
     axios.put(process.env.REACT_APP_API_BASEURL + `users/${user?.id}`, Data);
   };
 
+  const handleFavoriteToggle = () => {
+    const favorites = user?.favorites.favoriteitems || [];
+    const favitem = favorites.find((item) => item.id === product.id);
+    if (favitem) {
+      const index = favorites.findIndex((item) => item.id === product.id);
+      if (index !== -1) {
+        favorites.splice(index, 1);
+      }
+    } else {
+      favorites.push({
+        id: product.id,
+        title: product.title,
+        image: product.image,
+        amount: product.amount,
+        quantity: 1,
+      });
+    }
+    const Data: FavoritesData = {
+      email: user?.email || "",
+      favoriteitems: favorites,
+    };
+    updateFavorites(Data);
+    axios.put(process.env.REACT_APP_API_BASEURL + `favourites/${user?.id}`, Data);
+  };
+
   return (
     <div className="flex flex-col md:flex-row justify-around w-full items-center p-4">
       <div className="w-fit md:max-w-[40vw] flex justify-center">
@@ -76,8 +119,8 @@ export default function ProductPage() {
           </h1>
           <FaHeart
             id="favorite"
-            className="h-6 w-auto hover:cursor-pointer text-gray-500"
-            // onClick={handleFavoriteToggle}
+            className={`h-6 w-auto hover:cursor-pointer ${isFavorite ? "text-red-600" : "text-gray-500"}`}
+            onClick={handleFavoriteToggle}
           />
         </div>
         <p className="text-sm text-gray-500 mt-4">{product.description}</p>
@@ -87,7 +130,7 @@ export default function ProductPage() {
             className="bg-[#0478ee] text-white px-4 py-2 rounded-lg hover:cursor-pointer"
             onClick={handleAddItemCart}
           >
-            Add to Cart
+            {cartStatus ? "Remove from Cart" : "Add to Cart"}
           </button>
           <button className="bg-[#0478ee] text-white px-4 py-2 rounded-lg hover:cursor-pointer">
             Buy Now
